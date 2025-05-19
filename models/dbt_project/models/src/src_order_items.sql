c73a79ce0334b7924b5b6a6eae2cd86f8d3f9c62
AffanZafar <affanzafar002@gmail.com>
2025-05-19 14:31:38 +0500

    New Models

---
+++ models/dbt_project/models/src/src_order_items.sql
@@ -0,0 +1,22 @@
+with
+
+source as (
+
+    select * from {{ source('dlt_rest_api', 'raw_items') }}
+
+),
+
+renamed as (
+
+    select
+
+        ----------  ids
+        id as order_item_id,
+        order_id,
+        sku as product_id
+
+    from source
+
+)
+
+select * from renamed
